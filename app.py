72c942c0dd8b885821ab825cc71b1e8fd7c99ef1
praneethkoduru <40730409+praneethkoduru@users.noreply.github.com>
2024-06-01 17:12:17 +0000

    app ver2

--- app.py
+++ app.py
@@ -1,12 +1,56 @@
-from flask import Flask
+import flask_sqlalchemy
+from flask import Flask, flash, redirect, render_template, request, url_for
+from flask_sqlalchemy import SQLAlchemy
+from werkzeug.security import check_password_hash, generate_password_hash
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'your_secret_key'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
+db = SQLAlchemy(app)
 
-@app.route("/")
-def hello_world():
-    return "Hello world!"
 
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(150), unique=True, nullable=False)
+    password = db.Column(db.String(150), nullable=False)
+    def __init__(self, username, password):
+        self.username = username
+        self.password = password
+@app.route('/')
+def home():
+    return render_template('index.html')
 
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        user = User.query.filter_by(username=username).first()
+        if user and check_password_hash(user.password, password):
+            flash('Logged in successfully!')
+            return redirect(url_for('home'))
+        else:
+            flash('Invalid credentials')
+    return render_template('login.html')
+
+@app.route('/signup', methods=['GET', 'POST'])
+def signup():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        hashed_password = generate_password_hash(password, method='sha256')
+        new_user = User(username=username, password=hashed_password)
+        try:
+            db.session.add(new_user)
+            db.session.commit()
+            flash('User created successfully!')
+            return redirect(url_for('login'))
+        except flask_sqlalchemy.exc.IntegrityError:
+                 flash('User already exists!')
+            
+    return render_template('signup.html')
 
 if __name__ == '__main__':
-       app.run(debug=True)
+    with app.app_context():
+     
+         app.run(debug=True)
